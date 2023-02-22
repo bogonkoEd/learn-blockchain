12fe2868a59e775bed453b840e59b7f3f5017626
Bogonko Eddy <99894101+bogonkoEd@users.noreply.github.com>
2023-02-22 14:45:02 +0300

    Create a Block

---
+++ Block.js
@@ -0,0 +1,8 @@
+class Block {
+      constructor(blockchain, parentHash, nonce = sha256(new Date().getTime().toString()).toString()) {
+          this.blockchain = blockchain;
+              this.nonce = nonce;
+                  this.parentHash = parentHash;
+                     this.hash = sha256(this.nonce + this.parentHash).toString()
+                        }
+}
