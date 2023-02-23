ad842c9427358c6b4dc519996894daee5a632600
Bogonko Eddy <99894101+bogonkoEd@users.noreply.github.com>
2023-02-23 18:07:58 +0300

    Proof-of-work

--- Block.js
+++ Block.js
@@ -13,3 +13,25 @@ By pointing via hashes it is possible to enforce a specific order of blocks
 Changing a block would change it's hash and subsequently all blocks that are descendents of the block. (Following blocks)
 The last block validates all data in the ascendent blocks. 
  */ 
+
+class Block {
+      isValid() {
+          return this.parentHash === 'root' ||
+                (this.hash.substr(-DIFFICULTY) === "0".repeat(DIFFICULTY) &&
+                      this.hash === sha256(this.nonce + this.parentHash).toString())
+                        }
+
+                        setNonce(nonce) {
+                              this.nonce = nonce
+                                  this._setHash()
+                                    }
+
+                                      _setHash() {
+                                          this.hash = sha256(this.nonce + this.parentHash).toString()
+                                            }
+                                            }
+
+/*
+Proof-of-work is what "secures" the blockchain, makes it decentralized.
+The security of the blockchain relies on computational power to not be centralized within single parties.
+*/
